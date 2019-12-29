368e1e2a2a938c763f6e6e29a88cba47a456c978
yuto-yuto <yuto.nakatsu@gmail.com>
2019-12-29 12:58:46 +0100

    Commit test

---
+++ HelloWorld.py
@@ -0,0 +1,32 @@
+"""
+This is comment section
+"""
+
+
+def display_hello_world():
+    """
+    This is a function to display "Hello World!"
+    :return: void
+    """
+    print("Hello World!")
+
+
+display_hello_world()
+
+
+# Set text
+texts = ["Hello", "World", "!"]
+for text in texts:
+    print(text)
+
+numberString = "917254"
+for char in numberString:
+    number = int(char, 10)
+    if number > 5:
+        print("The number ", number, " is greater than 5.")
+    elif 5 == number:
+        print("The number is 5.")
+    else:
+        print("The number ", number, " is less than 5.")
+
+print("End")
