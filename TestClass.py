d880d427be0e63deb954f8101907fd0dc538e4e6
yuto-yuto <yuto.nakatsu@gmail.com>
2019-12-31 08:39:07 +0100

    Update

---
+++ TestClass.py
@@ -0,0 +1,9 @@
+class TestClass:
+    def __init__(self):
+        self.__text = "Good morning."
+        print("TestClass is created.")
+
+    def greet(self):
+        print("SAY: ", self.__text)
+
+
